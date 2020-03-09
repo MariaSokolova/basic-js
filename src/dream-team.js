8848d5894f2bbb8e2193aa0a0490d22cd2186a29
Maria Sokolova <sb_mv@inbox.ru>
2020-03-09 22:18:42 +0100

    feat: implement dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+    if (!Array.isArray(members)) {
+        return false
+    }
+
+    return members
+        .filter(el => typeof (el) === 'string')
+        .map((el => el
+            .trim()
+            .charAt(0)
+            .toUpperCase()))
+        .sort()
+        .join('');
 };

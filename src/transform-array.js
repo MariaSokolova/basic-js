a987ce77b5d4a6e42603db92b0db94687d477ba6
Maria Sokolova <sb_mv@inbox.ru>
2020-03-10 22:14:00 +0100

    feat: implement transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,34 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+    if (!Array.isArray(arr)) {
+        throw new Error();
+    }
+
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '--double-next' && i === arr.length - 1) {
+            continue;
+        } else if (arr[i] === '--double-next') {
+            newArr.push(arr[i + 1]);
+            continue;
+        }
+
+        if (arr[i] === '--discard-next') {
+            i++;
+            continue;
+        }
+        if (arr[i] === '--double-prev' && i === 0) {
+            continue;
+        } else if (arr[i] === '--double-prev') {
+            newArr.push(arr[i - 1]);
+            continue;
+        }
+        if (arr[i] === '--discard-prev') {
+            newArr.pop();
+            continue;
+        }
+        newArr.push(arr[i]);
+    }
+    return newArr;
 };

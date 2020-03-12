d92b64cd20b6f2a95c77c65c9038ecf464a3db1b
Maria Sokolova <sb_mv@inbox.ru>
2020-03-13 00:08:38 +0100

    feat: implement what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,21 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (!date) {
+        return 'Unable to determine the time of year!';
+    }
+    if (!(date instanceof Date)) {
+        throw new Error();
+    }
+    date.toLocaleString();
+
+    const month = date.getMonth();
+
+    if (month < 2 || month === 11) {
+        return 'winter';
+    } else if (month < 5) {
+        return 'spring';
+    } else if (month < 8) {
+        return 'summer';
+    } else if (month < 11) {
+        return 'fall';
+    }
 };

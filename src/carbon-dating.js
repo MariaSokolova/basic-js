d08cc803989e4c942f03cf7390f23eb8e4f66951
Maria Sokolova <sb_mv@inbox.ru>
2020-03-15 19:18:27 +0100

    feat: implement carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,19 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string'){
+    return false;
+  }
+
+  const numberActivity = parseFloat(sampleActivity);
+  if (numberActivity !== numberActivity){
+    return false;
+  }
+
+  if (numberActivity <= 0 || numberActivity > 15) {
+    return false;
+  }
+
+  return Math.ceil(Math.log((MODERN_ACTIVITY / numberActivity)) / (0.693 / HALF_LIFE_PERIOD));
 };

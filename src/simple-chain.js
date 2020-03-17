cdb26b3af40f2fffbb044230ccfa9d50bf7e5478
Maria Sokolova <sb_mv@inbox.ru>
2020-03-18 00:25:24 +0100

    feat: implement simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,24 +1,33 @@
 const chainMaker = {
-  getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  }
+    arr: [],
+    getLength() {
+        return this.arr.length;
+    },
+    addLink(value) {
+        this.arr.push(value);
+        return this;
+    },
+    removeLink(position) {
+        if (position <= 0 || position > this.arr.length - 1 || typeof (position) !== 'number') {
+          this.arr = [];
+            throw new Error();
+        }
+        this.arr.splice(position - 1, 1);
+        return this;
+    },
+    reverseChain() {
+        this.arr.reverse();
+        return this;
+    },
+    finishChain() {
+        let chain = '';
+        for (let i = 0; i < this.arr.length - 1; i++) {
+            chain += `( ${this.arr[i]} )~~`;
+        }
+        let fullChain = chain + `( ${this.arr[this.arr.length - 1]} )`;
+        this.arr = [];
+        return fullChain;
+    }
 };
 
 module.exports = chainMaker;
